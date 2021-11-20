afbf6a01e8d57fcafce547effc2e2c07bf2f88ef
owenagnel <72980369+owenagnel@users.noreply.github.com>
2021-11-20 20:04:50 +0000

    Update Enigma.scala

--- Enigma.scala
+++ Enigma.scala
@@ -14,9 +14,9 @@ object Enigma{
   /** Encrypts/Decrypts the text according to the given settings.*/
   def crypt(rotors: Array[Int], positions: Array[Int], notches: Array[Int], text: Array[Char]): Array[Char] = {
     // set the rotor position and notch
-    val rotor1 = Rotor(rotors(2), notches(0), positions(0))
-    val rotor2 = Rotor(rotors(1), notches(1), positions(1))
-    val rotor3 = Rotor(rotors(0), 0, positions(2))
+    val rotor1 = Rotor(rotors(2), notches(1), positions(2))
+    val rotor2 = Rotor(rotors(1), notches(0), positions(1))
+    val rotor3 = Rotor(rotors(0), 0, positions(0))
     //initialise output array
     val out = new Array[Char](text.length)
     var i = 0
